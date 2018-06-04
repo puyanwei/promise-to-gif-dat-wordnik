ce7871655fdac8aecff884b4b8f470c574c2bc34
Puyan Wei <puyanwei@gmail.com>
2018-06-04 20:37:07 +0100

    Put API key into a parameter

--- src/testing.js
+++ src/testing.js
@@ -1,7 +1,8 @@
-let giffyKey = "eUE4x88RaPqfRC2AN3ryVBIeZu4cr24F"
-let giffyAddress = "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key="
 
-let giffyAPICall = (giffyAddress +  giffyKey + "&limit=5");
+function giffyAPICall(key){
+    let giffyAddress = "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=";
+    giffyAddress +  giffyKey + "&limit=5";
+}
 
-var xhr = $.get(giffyAPICall);
+var xhr = $.get(giffyAPICall(key));
 xhr.done(function(data) { console.log("success got data", data); });
