183a5bc27dad5db11ca4dd640fd3a78e333c6389
Puyan Wei <puyanwei@gmail.com>
2018-06-04 23:00:04 +0100

    Search box added that return related gifs

--- src/testing.js
+++ src/testing.js
@@ -1,9 +1,9 @@
 
 function getGiffy(){
-    let key = $("input").val();
-    let giffyAddress = "http://api.giphy.com/v1/gifs/search?q=trending&api_key=";
-// https://api.giphy.com/v1/gifs/search?q=random&api_key=eUE4x88RaPqfRC2AN3ryVBIeZu4cr24F
-    var xhr = $.get(giffyAddress +  key + "&limit=50");
+    const gifKey = $("input:nth(1)").val();
+    const query = $("input:nth(0)").val();
+    const url = `http://api.giphy.com/v1/gifs/search?q=random+${query}&api_key=${gifKey}`;
+    const xhr = $.get(url + "&limit=50");
     xhr.done(function(data) { outputImgSrc(data); });
 }
 
@@ -11,8 +11,6 @@ function outputImgSrc(data){
     console.log(data);
     let randomiser = Math.floor(Math.random() * 50)
     let id = data.data[randomiser].id;
-    let imgSrc = `<img src="https://media.giphy.com/media/${id}/giphy.gif" width="500">`
+    let imgSrc = `<img src="https://media.giphy.com/media/${id}/giphy.gif" width="350">`
     $("section").append($(imgSrc))
 }
-
-// http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&minLength=5&maxLength=15&limit=1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5
