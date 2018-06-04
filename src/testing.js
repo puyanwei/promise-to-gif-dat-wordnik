a5d074584e000cc04ffaa4914ac6a187d661c5f2
Puyan Wei <puyanwei@gmail.com>
2018-06-04 21:16:11 +0100

    Linked API Key textbox to function

--- src/testing.js
+++ src/testing.js
@@ -1,8 +1,9 @@
 
-function giffyAPICall(key){
+function getGiffy(){
+    let key = $("input").val();
     let giffyAddress = "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=";
-    giffyAddress +  giffyKey + "&limit=5";
+    var xhr = $.get(giffyAddress +  key + "&limit=5");
+    xhr.done(function(data) { console.log("success got data", data); });
 }
 
-var xhr = $.get(giffyAPICall(key));
-xhr.done(function(data) { console.log("success got data", data); });
+// http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&minLength=5&maxLength=15&limit=1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5
