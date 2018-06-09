ba4179c168382b81fa3205b7fdfec44fb4d817d6
Puyan Wei <puyanwei@gmail.com>
2018-06-09 18:56:43 +0100

    Refactored API calls and parsing onto page into different functions, used a controller to call them all, adjusted some basic styling

--- src/getAPIs.js
+++ src/getAPIs.js
@@ -1,29 +1,35 @@
-function getGiffy(){
-  const query = getRandomWord() //wordnikAPI
-  const url = `http://api.giphy.com/v1/gifs/search?q=random+${query}&api_key=${giffyAPI}`;
-  const xhr = $.get(url + "&limit=50");
-  xhr.done((data) => outputImgSrc(data));
+async function controller() {
+  const word = await getRandomWord();
+  const giffyId = await getGiffy(word);
+  outputWord(word);
+  outputImgSrc(giffyId);
 }
 
-function outputImgSrc(data){
-  let randomiser = Math.floor(Math.random() * 50)
-  let id = data.data[randomiser].id;
-  let imgSrc = `<img src="https://media.giphy.com/media/${id}/giphy.gif" width="350">`
-  $("section").append($(imgSrc))
+async function getGiffy(query) {
+  const randomiser = Math.floor(Math.random() * 50);
+  const url = `http://api.giphy.com/v1/gifs/search?q=random+${query}&api_key=${giffyAPI}&limit=50`;
+  const response = await fetch(url);
+  const giffyData = await response.json();
+  const giffyId = await giffyData.data[randomiser].id;
+  return giffyId;
 }
 
-async function getRandomWord(){
+async function getRandomWord() {
   let randomWord;
-  const url = `https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=${wordnikAPI}`
+  const url = `https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=${wordnikAPI}`;
 
   await fetch(url)
-    .then(response => response.json())
-    .then(data => randomWord = data.word)
-    .then(word => outputWord(word))
-    .catch(err => console.log(err));
+    .then((response) => response.json())
+    .then((data) => (randomWord = data.word))
+    .catch((err) => console.log(err));
   return randomWord;
 }
 
 function outputWord(query) {
-  $("section").append($(`<p>${query}</p>`))
+  $('section').prepend($(`<h3>${query}</h3>`));
+}
+
+function outputImgSrc(id) {
+  let imgSrc = `<img src="https://media.giphy.com/media/${id}/giphy.gif" width="350">`;
+  $('section').prepend($(imgSrc));
 }
